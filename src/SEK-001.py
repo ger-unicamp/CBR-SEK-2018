b20567cec43a2f9b7bcf3d96860c7204a212ff4e
Tiago Hatta <tiago.eidi@gmail.com>
2018-05-25 16:15:28 -0300

    Codigo upa para ev3dev atualizado

--- src/SEK-001.py
+++ src/SEK-001.py
@@ -1,9 +1,10 @@
-import ev3dev.ev3 as ev3
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
 from time import sleep
 
 # definicao de motores
-motorDireita = LargeMotor('outA')
-motorEsquerda = LargeMotor('outB')
+motorDireita =LargeMotor('outB')
+motorEsquerda =LargeMotor('outA')
 motorGarra = MediumMotor('outC')
 
 # definicao de sensores
@@ -16,9 +17,8 @@ assert gyro.connected, "Giroscopio nao conectado"
 
 sensorCor = ColorSensor()
 assert sensorCor.connected, "Sensor de cor nao conectado"
-sensorCor.mode(COL-COLOR)
+sensorCor.mode='COL-COLOR'
 colors=('unknown','black','blue','green','yellow','red','white','brown')
-# print(colors[sensorCor.value()])
 
 def calibraGyro():
     gyro.mode = 'GYRO-RATE'
@@ -32,52 +32,50 @@ def girarRobo(anguloDesejado):
 
     if(anguloDesejado > 0):
         while(anguloSensor < anguloDesejado - 4): # 4 eh para compensar o lag de leitura do gyro
-            motorDireita.run_forever(speed_sp=-100)
-            motorEsquerda.run_forever(speed_sp=100)
-            sleep(0.5)
+            motorDireita.run_forever(speed_sp=-50)
+            motorEsquerda.run_forever(speed_sp=50)
             anguloSensor = gyro.value()
-
     else:
         while(anguloSensor > anguloDesejado + 4): # 4 eh para compensar o lag de leitura do gyro
-            motorDireita.run_forever(speed_sp=100)
-            motorEsquerda.run_forever(speed_sp=-100)
-            sleep(0.5)
+            motorDireita.run_forever(speed_sp=50)
+            motorEsquerda.run_forever(speed_sp=-50)
             anguloSensor = gyro.value()
 
     motorDireita.stop(stop_action="hold")
     motorEsquerda.stop(stop_action="hold")
-    sleep(1)
     calibraGyro()
     
 def agarrarBoneco():
     girarRobo(90)
+    sleep(2)
+    motorGarra.run_to_rel_pos(position_sp=220, speed_sp=100, stop_action="hold")
+    sleep(1)
     motorDireita.run_timed(time_sp=2000, speed_sp=200)
     motorEsquerda.run_timed(time_sp=2000, speed_sp=200)
-    sleep(5)
-    motorGarra.run_to_rel_pos(position_sp=220, speed_sp=100, stop_action="hold")
-    sleep(5)
+    sleep(3)
+    motorGarra.run_to_rel_pos(position_sp=-270, speed_sp=100, stop_action="hold")
+    sleep(1)
     motorDireita.run_timed(time_sp=2000, speed_sp=-200)
-    motorEsquerda.run__timed(time_sp=2000, speed_sp=-200)
-    sleep(5)
+    motorEsquerda.run_timed(time_sp=2000, speed_sp=-200)
+    sleep(3)
     girarRobo(-90)
     calibraGyro()
 
 def main():	
     btn = Button()
     calibraGyro()	
-    ev3.Sound.speak('Hello Humans!').wait()
+    Sound.speak('Hello Humans!').wait()
     while btn.any()==False:
         distancia = ultrassonico.value()/10  # converte de mm para cm
         if(distancia < 20):
-            motorDireita.run_forever(speed_sp=300)
-            motorEsquerda.run_forever(speed_sp=300)
-            sleep(0.5)
+            motorDireita.run_forever(speed_sp=200)
+            motorEsquerda.run_forever(speed_sp=200)
         else:
             motorDireita.stop(stop_action="hold")
             motorEsquerda.stop(stop_action="hold")
-            sleep(2)
             agarrarBoneco()
-    ev3.Sound.speak('Bye bye!').wait()
+    motorDireita.stop(stop_action="hold") #tentativa de parada
+    motorEsquerda.stop(stop_action="hold")
 
 if __name__ == "__main__":
     main()
