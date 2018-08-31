c47b4ebb3a1d2e2b9cfbea065ab8a2df331f9a2d
thaisaraujo <thais.araujo.hp@hotmail.com>
2018-08-31 14:39:32 -0300

    criação das pastas: sensores e estados

---
+++ src/sensores/cor/SEK-color.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+from time import sleep
+
+# Connect EV3 color sensor and check connected.
+
+c1 = ColorSensor()
+assert c1.connected, "Connect a color sensor to any sensor port"
+
+# Put the color sensor into COL-REFLECT mode
+# to measure reflected light intensity.
+# In this mode the sensor will return a value between 0 and 100
+c1.mode='COL-COLOR'
+
+while True:
+    if c1.value()==0:
+        print('none')
+        sleep(5)
+    elif c1.value()==1:
+        print('black')
+    elif c1.value()==2:
+        print('blue')
+    elif c1.value()==3:
+        print('green')
+    elif c1.value()==4:
+        print('yellow')
+    elif c1.value()==5:
+        print('red')
+    elif c1.value()==6:
+        print('white')
+    elif c1.value()==7:
+        print('brown')
+
+# I get max 80 with white paper, 3mm separation
+# and 5 with black plastic, same separation
