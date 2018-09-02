537e5e0b5454774e7f67da64f7fa07a533d60a0c
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2018-09-02 09:30:49 -0300

    Update stack_list.py

--- src/stack tests/stack_list.py
+++ src/stack tests/stack_list.py
@@ -1,8 +1,9 @@
-class Pilha:
+from dados import Dados
+class Pilha(Dados):
     def __init__(self):
         self.pilha = []
-    def push(self, direction):
-        self.pilha.append(direction)
+    def push(self, dados):
+        self.pilha.append(dados)
     def pop(self):
         if(len(self.pilha)!=0):
             # return self.pilha[len(self.pilha)-1]
@@ -12,13 +13,3 @@ class Pilha:
             return self.pilha[len(self.pilha)-1]
     def __str__(self):
         return f'{self.pilha}'
-
-pilha = Pilha()
-pilha.push("dir")
-pilha.push("esq")
-pilha.push("r")
-pilha.push("esq")
-print(pilha)
-pilha.pop()
-print(pilha.peek())
-print(pilha)
