781493ec25928f2c38e91227d63bd048f2bb8902
thaisaraujo <thais.araujo.hp@hotmail.com>
2018-09-02 10:15:57 -0300

    classe dados

---
+++ src/stack tests/dados.py
@@ -0,0 +1,17 @@
+class Dados:
+	def _init_(self, dir, cor):  # variavéis que podem ser do tipo inteiro
+	self.dir = dir
+	self.idade = idade
+
+	# métodos set(aterar) e set (consultar)
+	def setDir(self,dir):
+		self.dir = dir
+
+	def setCor(self, cor):
+		self.cor=cor
+
+	def getDir(self):
+		return self.dir
+
+	def getCor(self):
+		return self.cor
