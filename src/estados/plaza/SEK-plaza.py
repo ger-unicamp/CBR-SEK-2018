6e249527c64fb77fc87934ad045ba5c578edd76c
Émerson Takematsu <emersontakematsu@gmail.com>
2018-09-19 23:27:25 -0300

    upload do codigo plaza 19/09

--- src/estados/plaza/SEK-plaza.py
+++ src/estados/plaza/SEK-plaza.py
@@ -3,8 +3,8 @@ from ev3dev.ev3 import *
 from time import sleep
 
 # definicao de motores
-motorDireita = LargeMotor('outB')
-motorEsquerda = LargeMotor('outC')
+motorDireita = LargeMotor('outC')
+motorEsquerda = LargeMotor('outB')
 motorGarra = MediumMotor('outA')
 
 # definicao de sensores
@@ -31,11 +31,14 @@ def calibraGyro():
 	sleep(1)
 
 def girarRobo(anguloDesejado):
+	sleep(5)
 	calibraGyro()
 	anguloSensor = gyro.value()
+	print ("Angulo do giro: ", anguloSensor)
 
 	if(anguloDesejado > 0):
 		while(anguloSensor < anguloDesejado - 4): # 4 eh para compensar o lag de leitura do gyro
+			print ("Angulo do giro: ", anguloSensor)
 			motorDireita.run_forever(speed_sp=-50)
 			motorEsquerda.run_forever(speed_sp=50)
 			anguloSensor = gyro.value()
@@ -51,42 +54,59 @@ def girarRobo(anguloDesejado):
 
 def plaza_entrega_boneco ():
 	#Entrega do boneco
-	while (colors[SensorCorDir.value()] != "black") and (colors[SensorCorEsq.value()] != "black"):
-		motorDireita.run_forever(speed_sp=50)
-		motorEsquerda.run_forever(speed_sp=50)
-	sleep(1) #Esse sleep serve para não ficar na borda do centro preto
-	motorDireita.stop(stop_action="hold")
-	motorEsquerda.stop(stop_action="hold")
+	print("PLAZA AI VAMOS NOS")
+	while(colors[SensorCorDir.value()] != "black") or (colors[SensorCorEsq.value()] != "black"):
+		motorDireita.run_forever(speed_sp=200)
+		motorEsquerda.run_forever(speed_sp=200)
+	print("ACHEI PRETO, VOU IR PRO CENTRO DA BOLA")
+	sleep(2) #Esse sleep serve para não ficar na borda do centro preto
+	motorDireita.run_forever(speed_sp=0)
+	motorEsquerda.run_forever(speed_sp=0)
 	motorGarra.run_to_rel_pos(position_sp=290, speed_sp=100, stop_action="hold") #abre H ra
-	motorDireita.run_timed(time_sp=1400, speed_sp=-50)
-	motorEsquerda.run_timed(time_sp=1400, speed_sp=-50)
+	print("AGORA VOU ANDAR PRA TRAS")
+	sleep(2)
+	motorDireita.run_timed(time_sp=1400, speed_sp=-200)
+	motorEsquerda.run_timed(time_sp=1400, speed_sp=-200)
 	sleep(3)
 	motorGarra.run_to_rel_pos(position_sp=-300, speed_sp=100, stop_action="hold") #fecha H ra
+	sleep(1)
+	plaza_modo1()
 
 def plaza_modo1():
-	giraRobo (180)
+	print("SAINDO DO PLAZA")
 	motorDireita.run_timed(time_sp=2000, speed_sp=200)
 	motorEsquerda.run_timed(time_sp=2000, speed_sp=200)
-	motorDireita.stop(stop_action="hold")
-	motorEsquerda.stop(stop_action="hold")
+	motorDireita.run_forever(speed_sp=0)
+	motorEsquerda.run_forever(speed_sp=0)
+	sleep(1)
 	#Verifica distancia das paredes:
-	dist_esq = UltrasonicSensor()
-	giraRobo (180)
 	dist_dir = UltrasonicSensor()
-	girarRobo (180)
-	if (dist_esq - dist_dir < 3):
-		motorDireita.run_timed(time_sp=2000, speed_sp=200)
-		motorEsquerda.run_timed(time_sp=2000, speed_sp=200)
+	girarRobo(180)
+	dist_esq = UltrasonicSensor()
+	girarRobo(180)
+	while(dist_esq - dist_dir < 3) or (dist_dir - dist_esq < 3):
+		if(dist_esq > dist_dir):
+			#Anda para a esquerda
+			girarRobo(-90)
+			motorDireita.run_timed(time_sp=5*(dist_esq - dist_dir), speed_sp=50)
+			motorEsquerda.run_timed(time_sp=5*(dist_esq - dist_dir), speed_sp=50)
+			girarRobo(90)
+		else:
+			#Anda para a direita
+			girarRobo(90)
+			motorDireita.run_timed(time_sp=5*(dist_esq - dist_dir), speed_sp=50)
+			motorEsquerda.run_timed(time_sp=5*(dist_esq - dist_dir), speed_sp=50)
+			girarRobo(-90)
+	motorDireita.run_timed(time_sp=2000, speed_sp=200)
+	motorEsquerda.run_timed(time_sp=2000, speed_sp=200)
 
 
 
 def main():
-	while (True):
-		print ("Hello")
-		sleep(0.1)
-		motorDireita.run_forever(speed_sp=50)
-		motorEsquerda.run_forever(speed_sp=50)
-		sleep(1)
-		if (colors[SensorCorDir.value()] == "red") or (colors[SensorCorEsq.value()] != "red"):
-			plaza_entrega_boneco()
-			plaza_modo1()
+	motorDireita.run_forever(speed_sp=200)
+	motorEsquerda.run_forever(speed_sp=200)
+	if (colors[SensorCorDir.value()] == "red") or (colors[SensorCorEsq.value()] == "red"):
+		plaza_entrega_boneco()
+
+while(True):
+	main()
