35903bde85fbc5ec73b05e388e79e8a1f0f88d2c
IgorAmorimUnicamp <40005855+IgorAmorimUnicamp@users.noreply.github.com>
2018-10-04 17:10:18 -0300

    Update SEK alinhamento rampa_teste.py

--- src/estados/plaza/SEK alinhamento rampa_teste.py
+++ src/estados/plaza/SEK alinhamento rampa_teste.py
@@ -2,22 +2,27 @@
 from ev3dev.ev3 import *
 from time import sleep
 
-# Connect EV3 color sensor and check connected
-
 # definicao de motores
 motorDireita = LargeMotor('outC')
 motorEsquerda = LargeMotor('outB')
+motorGarra = MediumMotor('outA')
 
-#definicao dos sensores de cor
+# definicao de sensores
+ultrassonico = UltrasonicSensor()
+assert ultrassonico.connected, "Sensor ultrassonico nao conectado"
+ultrassonico.mode = 'US-DIST-CM'
+
+gyro = GyroSensor()
+assert gyro.connected, "Giroscopio nao conectado"
+
 SensorCorDir = ColorSensor('in1')
 assert SensorCorDir.connected, "Sensor de cor nao conectado"
 SensorCorDir.mode='COL-COLOR'
+colors=('none','black','blue','green','yellow','red','white','brown')
 
 SensorCorEsq = ColorSensor('in4')
 assert SensorCorEsq.connected, "Sensor de cor nao conectado"
 SensorCorEsq.mode='COL-COLOR'
-
-
 
 def rampaIda(): #subida do robo
 	print("estou na subida")
