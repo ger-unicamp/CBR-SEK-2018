784efe78669442edc433dd2d6a6a49fc6fca9f8b
natrodrigues <natanrodriguesdeoliveira@gmail.com>
2018-10-15 17:23:45 -0300

    Update do main.py

---
+++ src/main/interseccao.py
@@ -0,0 +1,44 @@
+'''
+GER - UNICAMP (17/09/2018)
+Autor: Natan Rodrigues
+
+NOTE: ISTO NAO EH UMA PILHA! Foi tanto falado em pilha, pelo seu funcionamento
+LIFO, mas em python decidimos criar um TAD (tipo abstrato de dados) que se encaixasse
+em nossas necessidades. Btw, ISTO NAO EH UMA PILHA.
+
+A ideia dessa classe (nome a definir) eh armazenar e processar informacoes sobre
+interseccao do labirinto da competicao. Os principais metodos (push, acessa, where_to_go),
+fazem isso. O push se encarrega do armazenamento, apos a validacao no codigo principal.
+O metodo acessa poderia estar dentro do where_to_go, entretanto para o computador
+nao faz diferenca, logo a separacao desses metodos se fez para deixar mais claro
+a diferenca de cada um e quando usar. O acessa eh usado quando ja sabemos sobre todo o
+labirinto, por isso o nome. Analogamente, o where_to_go, usado quando ainda nao sabemos.
+'''
+
+class Interseccao:
+    def __init__(self):
+        self.inter = {'2': -1, '3': -1, '5': -1}#as chaves sao as cores, comeca com -1 pq ainda n sabemosa  direcao
+        self.verify_dir = -1#variavel para verificacao da direcao a ser 'pushed'
+    def push(self, cor, direcao):
+        if(direcao==0):
+            self.inter['{}'.format(cor)]=self.verify_dir
+        else:
+            self.inter['{}'.format(cor)] = direcao
+        self.verify_dir =-1
+    def acessa(self, cor):
+        return self.inter['{}'.format(cor)]
+	def where_to_go(self, cor):
+		if self.iter['{}'.format(cor)] != -1:
+			return self.iter['{}'.format(cor)]
+		else:
+			if 0 not in self.inter.values():
+                self.verify_dir+=1
+                return 0
+			elif 1 not in self.inter.values() and self.verify_dir==-1:
+                self.verify_dir+=1
+                return 1
+            else:
+                self.verify_dir = 2
+                return 0
+    def __str__(self):
+        return '{}'.format(self.inter)
---
+++ src/main/main.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+from time import sleep
+from interseccao.py import Interseccao
+
+#variaveis globais e flags------------------------------------------------------
+colors=('none','black','blue','green','yellow','red','white','brown')
+has_boneco = False#variavel de verificacao para pegar ou nao bonecos
+labyrinth = False#para saber quando ja conhecemos o laribirinto (1a vez apenas)
+#path = list() #lista com as direcoes a serem seguidas pelo trajeto (path)
+times = 0 #quantidade de interseccoes passadas times p. [0,4]
+way = 1 #1 se for ida (direto) ao setido do plaza, 0 se for volta (contrario) ao sentido do plaza
+inter = Interseccao()
+number_of_inter = 4 # na FINAL trocar por 6
+
+# definicao de motores----------------------------------------------------------
+motorDireita = LargeMotor('outC')
+motorEsquerda = LargeMotor('outB')
+motorGarra = MediumMotor('outA')
+
+# definicao de sensores---------------------------------------------------------
+ultrassonico = UltrasonicSensor()
+assert ultrassonico.connected, "Sensor ultrassonico nao conectado"
+ultrassonico.mode = 'US-DIST-CM'
+
+SensorCorDir = ColorSensor('in1')
+assert SensorCorDir.connected, "Sensor de cor nao conectado"
+SensorCorDir.mode='COL-COLOR'
+
+SensorCorEsq = ColorSensor('in4')
+assert SensorCorEsq.connected, "Sensor de cor nao conectado"
+SensorCorEsq.mode='COL-COLOR'
+
+gyro = GyroSensor()
+assert gyro.connected, "Giroscopio nao conectado"
+
+#funcoes (e estados) -------------------------------------------------------------
+def calibraGyro():
+    gyro.mode = 'GYRO-RATE'
+    sleep(1)
+    gyro.mode = 'GYRO-ANG'
+    sleep(1)
+def troca():
+    if way ==1: way=0
+    else: way=1
+    if times==4 and not labyrinth: labyrinth=True
+def girarRobo(anguloDesejado):
+    calibraGyro()
+    anguloSensor = gyro.value()
+    if(anguloDesejado > 0):
+        while(anguloSensor < anguloDesejado - 4): # 4 eh para compensar o lag de leitura do gyro
+            motorDireita.run_forever(speed_sp=-50)
+            motorEsquerda.run_forever(speed_sp=50)
+            anguloSensor = gyro.value()
+    else:
+        while(anguloSensor > anguloDesejado + 4): # 4 eh para compensar o lag de leitura do gyro
+            motorDireita.run_forever(speed_sp=50)
+            motorEsquerda.run_forever(speed_sp=-50)
+            anguloSensor = gyro.value()
+
+    motorDireita.stop(stop_action="hold")
+    motorEsquerda.stop(stop_action="hold")
+    calibraGyro()
+def agarrarBoneco():
+    motorDireita.run_timed(time_sp=500, speed_sp=200)
+    motorEsquerda.run_timed(time_sp=500, speed_sp=200)
+    sleep(2)
+    girarRobo(90)
+    sleep(2)
+    motorGarra.run_to_rel_pos(position_sp=290, speed_sp=100, stop_action="hold")
+    sleep(5)
+    motorDireita.run_timed(time_sp=1400, speed_sp=200)
+    motorEsquerda.run_timed(time_sp=1400, speed_sp=200)
+    sleep(3)
+    motorGarra.run_to_rel_pos(position_sp=-290, speed_sp=100, stop_action="hold")#NOTE: TESTAR GARRAS
+    #de -100 a -330 a garra n subiu
+    sleep(5)
+    #como subir a garra?
+    motorDireita.run_timed(time_sp=1400, speed_sp=-200)
+    motorEsquerda.run_timed(time_sp=1400, speed_sp=-200)
+    sleep(3)
+    girarRobo(-90)
+    has_boneco = True
+    calibraGyro()
+def andarReto():
+    motorDireita.run_forever(speed_sp=200)
+    motorEsquerda.run_forever(speed_sp=200)
+    
+#funcao main -------------------------------------------------------------------
+def main():
+    aux = list()#auxiliar para ler adequadamente o ultrassonico
+    btn = Button()
+    calibraGyro()
+    Sound.speak('Hello Humans!').wait()
+    while not btn.any():
+        if colors(SensorCorDir.value()) == 'white' and colors(SensorCorEsq.value()) == 'white':
+            andarReto()#NOTE: VAI RODAR TODA VEZ QUE ESTIVER EM BRANCO?
+            aux = [ultrassonico.value()/10 for i in range(10)]
+            aux.sort()
+            if aux[4] < 20 and not has_boneco and labyrinth:
+                agarrarBoneco()
+                troca()
+            aux.clear()
+        #tratar pista
+        elif (colors(SensorCorDir.value()) == 'white' and colors(SensorCorEsq.value()) == 'black') or (colors(SensorCorDir.value()) == 'black' and colors(SensorCorEsq.value()) == 'white'):
+            alinhar()#TODO
+        elif colors(SensorCorDir.value()) != 'white' and colors(SensorCorDir.value()) != 'black' and colors(SensorCorEsq.value()) != 'white' and colors(SensorCorEsq.value()) != 'black':
+            if times < number_of_inter:
+                interseccao()#TODO
+            elif times == number_of_inter and labyrinth:
+                rampa()#TODO
+            elif times == number_of_inter and not labyrinth:
+                troca()
+        elif colors(SensorCorDir.value()) == 'black' and colors(SensorCorEsq.value()) == 'black':
+            volta()#TODO
+if __name__ == '__main__':
+    main()
