d0339e9087673af75057520b1a5fd28e453f1e26
Luis Felipe Brentegani <lf.brentegani@gmail.com>
2018-10-19 14:12:24 -0300

    Alinha C

--- src/estados/rua/SEK-alinha_rua.py
+++ src/estados/rua/SEK-alinha_rua.py
@@ -76,31 +76,31 @@ def manobra1():
 		motorEsquerda.run_timed(time_sp=1100, speed_sp=-200)
 		girarRobo(90)
 
-def alinha1(): #Começar função alinha com StopMotion
-	motorEsquerda.run_forever(speed_sp=0)
-	motorDireita.run_forever(speed_sp=0)
-	sleep(0.4)
-	while(colors[SensorCorDir.value()] != 'white') or (colors[SensorCorEsq.value()] != 'white'):
-		motorDireita.run_forever(speed_sp=-100)
-		motorEsquerda.run_forever(speed_sp=-100)
-	if(colors[SensorCorDir.value()] == 'white'):
-		while (colors[SensorCorEsq.value()] != 'white'):
-			motorDireita.run_forever(speed_sp=0) #STOP ACTION
-			motorEsquerda.run_forever(speed_sp=-180)
-	elif(colors[SensorCorEsq.value()] == 'white'):
-		while (colors[SensorCorDir.value()] != 'white'):
-			motorDireita.run_forever(speed_sp=-180) #STOP ACTION
-			motorEsquerda.run_forever(speed_sp=0)
-	motorEsquerda.run_forever(speed_sp=0)
-	motorDireita.run_forever(speed_sp=0)
-	exit()
+# def alinha1(): #Começar função alinha com StopMotion
+# 	motorEsquerda.run_forever(speed_sp=0)
+# 	motorDireita.run_forever(speed_sp=0)
+# 	sleep(0.4)
+# 	while(colors[SensorCorDir.value()] != 'white') or (colors[SensorCorEsq.value()] != 'white'):
+# 		motorDireita.run_forever(speed_sp=-100)
+# 		motorEsquerda.run_forever(speed_sp=-100)
+# 	if(colors[SensorCorDir.value()] == 'white'):
+# 		while (colors[SensorCorEsq.value()] != 'white'):
+# 			motorDireita.run_forever(speed_sp=0) #STOP ACTION
+# 			motorEsquerda.run_forever(speed_sp=-180)
+# 	elif(colors[SensorCorEsq.value()] == 'white'):
+# 		while (colors[SensorCorDir.value()] != 'white'):
+# 			motorDireita.run_forever(speed_sp=-180) #STOP ACTION
+# 			motorEsquerda.run_forever(speed_sp=0)
+# 	motorEsquerda.run_forever(speed_sp=0)
+# 	motorDireita.run_forever(speed_sp=0)
+# 	exit()
 
 def alinha():
 	motorEsquerda.run_forever(speed_sp=0)
 	motorDireita.run_forever(speed_sp=0)
 	while(colors[SensorCorDir.value()] != 'white') and (colors[SensorCorEsq.value()] != 'white'):
-		if(colors[SensorCorDir.value()] != 'white'):
-			motorDireita.run_forever(speed_sp=-50)
+		if(colors[SensorCorDir.value()] != 'white'): #Enquanto os dois não estiverem fora da interssecção ou fora do fim de pista, alinha os dois sensores
+			motorDireita.run_forever(speed_sp=-50) #em branco
 		else:
 			motorDireita.run_forever(speed_sp=0)
 		if(colors[SensorCorEsq.value()] != 'white'):
@@ -109,39 +109,42 @@ def alinha():
 			motorEsquerda.run_forever(speed_sp=0)
 	motorEsquerda.run_forever(speed_sp=0)
 	motorDireita.run_forever(speed_sp=0)
-	exit()
+	#exit()
 
 def main(): #TESTES
-	motorEsquerda.run_forever(speed_sp=200)
+	motorEsquerda.run_forever(speed_sp=200) #Começa o programa andando
 	motorDireita.run_forever(speed_sp=200)
 	print("Sensor direito:", colors[SensorCorDir.value()])
 	print("Sensor esquerdo:", colors[SensorCorEsq.value()])
-	if (colors[SensorCorDir.value()] != 'white') or (colors[SensorCorEsq.value()] != 'white'):
+	if (colors[SensorCorDir.value()] != 'white') or (colors[SensorCorEsq.value()] != 'white'): #Condição de saída de pista
 		if (colors[SensorCorDir.value()] == 'none') or (colors[SensorCorDir.value()] == 'black') or (colors[SensorCorEsq.value()] =='none') or (colors[SensorCorEsq.value()] == 'black'): #verificação Final da RUA
-			sleep(0.2)
-			if (colors[SensorCorDir.value()] == 'black') and (colors[SensorCorEsq.value()] =='black'):
+			sleep(0.2) #Esse IF verifica se está na borda ou no fim da rua. O SLEEP é pro robô andar um pouco pra ter certeza da condição
+			if (colors[SensorCorDir.value()] == 'black') and (colors[SensorCorEsq.value()] =='black'): #Condição Fim de rua (Pós sleep)
 				print("FIM DA RUA")
 				alinha()
+				motorEsquerda.run_forever(speed_sp=0)
+				motorDireita.run_forever(speed_sp=0)
+				sleep(0.3)
+				girarRobo(180)
 			elif (colors[SensorCorDir.value()] == 'none') or (colors[SensorCorDir.value()] == 'black') or (colors[SensorCorDir.value()] == 'brown') or (colors[SensorCorEsq.value()] =='none') or (colors[SensorCorEsq.value()] == 'black'):
-				print("BORA PRA MANOBRA")
+				print("BORA PRA MANOBRA") #Condição de beirada da pista
 				manobra1()
-		elif (colors[SensorCorDir.value()] == 'yellow') or (colors[SensorCorEsq.value()] =='yellow'):
-			sleep(0.2)
+		elif (colors[SensorCorDir.value()] == 'yellow') or (colors[SensorCorEsq.value()] =='yellow'): #interssecção amarela
+			sleep(0.2) #Sleep de garantia de condição
 			if (colors[SensorCorDir.value()] == 'yellow') and (colors[SensorCorEsq.value()] =='yellow'):
-				alinha()
-		elif (colors[SensorCorDir.value()] == 'red') or (colors[SensorCorEsq.value()] =='red'):
-			sleep(0.2)
+				alinha() #Enquanto os dois não forem brancos ele fica alinhando!
+				exit() #Estes exit são só a prévia para ver o funcionamento do programa. Sai do programa quando alinha de uma interssecção
+		elif (colors[SensorCorDir.value()] == 'red') or (colors[SensorCorEsq.value()] =='red'): #interssecção vermelha
+			sleep(0.2) #Sleep de garantia de condição
 			if (colors[SensorCorDir.value()] == 'red') and (colors[SensorCorEsq.value()] =='red'):
 				alinha()
-		elif (colors[SensorCorDir.value()] == 'green') or (colors[SensorCorEsq.value()] =='green'):
-			sleep(0.2)
+				exit()
+		elif (colors[SensorCorDir.value()] == 'green') or (colors[SensorCorEsq.value()] =='green'): #interssecção verde
+			sleep(0.2) #Sleep de garantia de condição
 			if (colors[SensorCorDir.value()] == 'green') and (colors[SensorCorEsq.value()] =='green'):
 				alinha()
-
+				exit()
 
 
 while(True):
 	main()
-
-
-
